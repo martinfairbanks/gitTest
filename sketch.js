2f55a289e9fd11fe113ab090c9168cbc3e36be3e
martinfairbanks <martinfbanks@gmail.com>
2017-02-19 15:09:32 +0100

    test

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var x = 0;
+
+function setup() {
+    createCanvas(640, 480);
+}
+
+function draw() {
+    changeBackground();
+    if (mouseIsPressed) {
+        fill(255,0,255);
+    } else {
+        fill(0,255,0);
+    }
+    
+    ellipse(mouseX, mouseY, 80, 80);
+}
+
+function changeBackground() {
+    //background(random(255), random(255), random(255));
+	background(200);
+	line(100,100,200,200);
+	ellipse(x,height/2,20,20);
+	x = x + 1;
+
+	drawingContext.shadowOffsetX = 5;
+	drawingContext.shadowOffsetY = 5;
+	drawingContext.shadowBlur = 10;
+	drawingContext.shadowColor = "black";
+	ellipse(width/2,height/2,50,50);
+
+	
+}
